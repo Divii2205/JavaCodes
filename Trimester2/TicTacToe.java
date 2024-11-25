58793a428eb2274a9a1b95cf45e0f1e18ac2f09c
Divii2205 <divijaa.24bcs10206@sst.scaler.com>
2024-11-25 12:57:44 +0530

    Add winning statergy

--- Trimester2/TicTacToe.java
+++ Trimester2/TicTacToe.java
@@ -19,27 +19,44 @@ public class TicTacToe {
 
 
     static void run(){
+        boolean draw = true;
         int turn = 0;
         while(turn<9){
-
 
             b.print();
             System.out.println(p1.name + " 's move");
             p1.makeMove(b.board);
+            if (b.winStat(p1.symbol)) {
+                System.out.println(p1.name + " won the game!");
+                draw = false;
+                break;
+            }
             turn++;
-           
+
+            if (turn == 9) break;
+
             b.print();
             System.out.println(p2.name + " 's move");
             p2.makeMove(b.board);
+            if (b.winStat(p2.symbol)) {
+                System.out.println(p2.name + " won the game!");
+                draw = false;
+                break;
+            }
             turn++;
         }
+        if (draw) System.out.println("DRAW!");
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter player1 name: ");
         String p1_name = sc.next();
+        System.out.print("Choose your symbol: ");
         char p1_symbol = sc.next().charAt(0);
-       
+        
+        System.out.print("Enter player2 name: ");
         String p2_name = sc.next();
+        System.out.print("Choose your symbol: ");
         char p2_symbol = sc.next().charAt(0);
        
         Game(p1_name, p1_symbol, p2_name, p2_symbol);
@@ -68,6 +85,27 @@ class  Board {
             }
             System.out.println();
         }
+        System.out.println();
+    }
+
+    boolean winStat(char symbol){
+        for (int i = 0; i<3; i++){
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol){
+                return true;
+            }
+        }
+        for (int i = 0; i<3; i++){
+            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol){
+                return true;
+            }
+        }
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol){
+            return true;
+        }
+        if (board[2][0] == symbol && board[1][1] == symbol && board[0][2] == symbol){
+            return true;
+        }
+        return false;
     }
 }
 
@@ -87,7 +125,7 @@ class Player{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-
+        System.out.println();
 
         board[n][m] = symbol;
     }
